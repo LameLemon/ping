9e762038e96e58310f23659c581396496df99189
lamelemon <peskypotato@protonmail.com>
2020-12-23 11:07:52 +0100

    Added arguments to checks.py
    - -c to go perform http check on services
    - -u to update database with the config file

--- checks.py
+++ checks.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from database import Database
 import requests
+import argparse
+import json
+
 
 def check_loop():
     db = Database("./ping.db")
@@ -10,6 +13,7 @@ def check_loop():
         db.insert_check(datetime.timestamp(datetime.now()), status, service[0])
         print(service[1], status)
 
+
 def http_check(url, status_code):
     r = requests.get(url)
     if r.status_code == status_code:
@@ -17,3 +21,46 @@ def http_check(url, status_code):
     print(r.status_code)
     return False
 
+
+def update_config():
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+
+    db = Database("./ping.db")
+    db.create_tables()
+
+    for category in data.get("categories", []):
+        cat_name = category.get("name")
+        if not db.check_category_by_name(cat_name):
+            db.insert_category(cat_name)
+
+        for service in category.get("services", []):
+            name = service.get("name")
+            if not db.check_service_by_name(name):
+                db.insert_service(
+                    name, service.get("url"),
+                    service.get("status_code"), cat_name
+                )
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", "--check",
+        help="check all services", action="store_true"
+    )
+    parser.add_argument(
+        "-u", "--update_config",
+        help="update database with new config", action="store_true"
+    )
+
+    args = parser.parse_args()
+    if args.check:
+        check_loop()
+
+    if args.update_config:
+        update_config()
+
+
+if __name__ == "__main__":
+    main()
